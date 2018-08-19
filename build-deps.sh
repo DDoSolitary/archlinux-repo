358019b101823f653509f26db1d032af49916393
DDoSolitary <DDoSolitary@gmail.com>
2018-08-19 21:26:57 +0800

    Update dependency info for ruby-connection_pool.

--- build-deps.sh
+++ build-deps.sh
@@ -1,8 +1,9 @@
 declare -xA deps
 deps=(
 	[yaourt]="package-query"
-	[ruby-travis]="ruby-backports ruby-connection_pool ruby-gh ruby-highline ruby-launchy ruby-pusher-client ruby-typhoeus-0.6"
+	[ruby-travis]="ruby-backports ruby-gh ruby-highline ruby-launchy ruby-pusher-client ruby-typhoeus-0.6"
 	[ruby-gh]="ruby-backports ruby-net-http-persistent ruby-net-http-pipeline"
+	[ruby-net-http-persistent]="ruby-connection_pool"
 	[ruby-pusher-client]="ruby-json ruby-websocket"
 	[ruby-typhoeus-0.6]="ruby-ethon"
 	[i2p]="java-service-wrapper"
