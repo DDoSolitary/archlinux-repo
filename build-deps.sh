c9757e6518c4746f1fd6672df789c0f5565826ed
DDoSolitary <DDoSolitary@gmail.com>
2020-05-01 22:11:25 +0800

    [ruby-travis] Update dependency information.

--- build-deps.sh
+++ build-deps.sh
@@ -1,10 +1,10 @@
 declare -xA deps
 deps=(
 	[yaourt]="package-query"
-	[ruby-travis]="ruby-backports ruby-faraday-0.17 ruby-gh ruby-highline-1.6 ruby-launchy ruby-pusher-client ruby-typhoeus-0.6"
+	[ruby-travis]="ruby-backports ruby-gh ruby-highline ruby-launchy ruby-pusher-client ruby-typhoeus-0.6"
 	[ruby-gh]="ruby-backports ruby-net-http-persistent ruby-net-http-pipeline"
 	[ruby-net-http-persistent]="ruby-connection_pool"
-	[ruby-pusher-client]="ruby-json ruby-websocket"
+	[ruby-pusher-client]="ruby-websocket"
 	[ruby-typhoeus-0.6]="ruby-ethon"
 	[i2p]="java-service-wrapper"
 	[i2p-dev]="java-service-wrapper"
